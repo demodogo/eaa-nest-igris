af242a3b18bbc7f6dbc7368fadb19606908a977d
demodogo <macaigm@gmail.com>
2025-12-29 20:09:15 -0300

    fix: fix tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,7 +4,7 @@ import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Application (e2e)', () => {
 	let app: INestApplication<App>;
 
 	beforeEach(async () => {
@@ -16,10 +16,22 @@ describe('AppController (e2e)', () => {
 		await app.init();
 	});
 
-	it('/ (GET)', () => {
-		return request(app.getHttpServer())
-			.get('/')
-			.expect(200)
-			.expect('Hello World!');
+	afterEach(async () => {
+		await app.close();
+	});
+
+	describe('/health (GET)', () => {
+		it('should return health status', () => {
+			return request(app.getHttpServer())
+				.get('/health')
+				.expect(200)
+				.expect((res) => {
+					expect(res.body).toHaveProperty('ok', true);
+					expect(res.body).toHaveProperty('timestamp');
+					expect(res.body).toHaveProperty('environment');
+					expect(res.body).toHaveProperty('uptime');
+					expect(res.body).toHaveProperty('version');
+				});
+		});
 	});
 });
